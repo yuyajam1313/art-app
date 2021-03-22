52d442646587b796ba1857f0ca8893fac4ac3cbb
yuyajam1313 <78283195+yuyajam1313@users.noreply.github.com>
2021-03-22 14:25:33 +0900

    コメント機能作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
+
+  with_options presence: true do
+    validates :visual_reading
+    validates :question
+    validates :answer
+  end
+
   belongs_to :user
   belongs_to :post
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "staticpages#index"
-  resources :posts, only: [:new, :create, :show]
+  resources :posts, only: [:index, :new, :create, :show] do
+    resources :comments, only: [:create]
+  end
 end
