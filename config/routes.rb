9bd7accb1294b705039bce7e55ec6c9c0d263efd
yuyajam1313 <78283195+yuyajam1313@users.noreply.github.com>
2021-03-28 21:59:53 +0900

    ユーザー投稿詳細ページのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "staticpages#index"
+  resources :staticpages, only:[:show]
   resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create]
   end
